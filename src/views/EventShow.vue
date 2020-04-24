1d63427957ab0804b0cb688531985d2e107e817d
Kriyo <krikenn@gmail.com>
2020-04-24 11:11:10 +0100

    extended api to get a single event

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Showing event #{{ id }}</h1>
+  <h1>Showing event {{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(res => {
+        this.event = res.data
+      })
+      .catch(err => {
+        console.log('::> Errors: ', err.response)
+      })
+  }
 }
 </script>
