fcaf0b2b81bcf7b2ea909fbd7a21d9dd3cc60d2f
Kriyo <krikenn@gmail.com>
2020-04-24 15:00:19 +0100

    added mapstate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <ul>
+      <li v-for="c in categories" :key="c">
+        {{ c }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    user: 'user',
+    categories: 'categories'
+  })
+}
+</script>
+
+<style lang="scss" scoped></style>
