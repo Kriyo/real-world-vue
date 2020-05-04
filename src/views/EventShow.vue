811227ae8c4b1ac4a548fa88d8cb7c6a10abc3d0
Kriyo <krikenn@gmail.com>
2020-05-04 20:03:14 +0100

    swapped out mapActions, created hook for an in-component lifecycle route guard, so the template content does not load until the data is present

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -19,13 +19,15 @@
     <h2>
       Attendees
       <span class="badge -fill-gradient">
-        {{
-        event.attendees ? event.attendees.length : 0
-        }}
+        {{ event.attendees ? event.attendees.length : 0 }}
       </span>
     </h2>
     <ul class="list-group">
-      <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
         <b>{{ attendee.name }}</b>
       </li>
     </ul>
@@ -33,17 +35,22 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+import store from '@/store/index.js'
 
 export default {
   props: ['id'],
-  created() {
-    this.fetchEvent(this.id)
+  beforeRouteEnter(routeTo, route, next) {
+    NProgress.start()
+    store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
+      NProgress.done()
+      next()
+    })
   },
   computed: mapState({
     event: state => state.event.event // shit node names but here we are.
-  }),
-  methods: mapActions('event', ['fetchEvent'])
+  })
 }
 </script>
 
