e990a4e7185e6142b7690f3110df496b0974f781
Kriyo <krikenn@gmail.com>
2020-05-05 12:04:08 +0100

    Expanded error handling for better networking, will differentiate the catch based on a 404 response

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import EventCreate from '../views/EventCreate.vue'
 import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
 import NotFound from '../views/NotFound.vue'
+import NetworkIssue from '../views/NetworkIssue.vue'
 import NProgress from 'nprogress'
 import store from '@/store'
 
@@ -33,7 +34,13 @@ const routes = [
           routeTo.params.event = event
           next()
         })
-        .catch(() => next({ name: '404', params: { resource: 'event ' } }))
+        .catch(error => {
+          if (error.response && error.response.status === 404) {
+            next({ name: '404', params: { resource: 'event ' } })
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
     }
   },
   {
@@ -41,6 +48,11 @@ const routes = [
     name: '404',
     component: NotFound,
     props: true
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: NetworkIssue
   },
   {
     path: '*',
